7b53b8d91e0a525cbe66dc830e5d4b57499ac023
BKN46 <34993220+BKN46@users.noreply.github.com>
2021-04-05 11:46:01 +0800

    redo

--- serialize_proc.py
+++ serialize_proc.py
@@ -1,13 +1,18 @@
+def strproc(a):
+    return a.replace('\t','').replace('\n','')
+
 if __name__ == "__main__":
     data={}
+    TAB='\t'
     with open("SimplifiedChinese.tsv", encoding="utf-8") as f:
         lines=f.readlines()
         for line in lines:
             seg = line.split("\t")
             data[seg[2]]=seg[3][:-1]
-    with open("new.tsv", encoding="utf-8") as f:
-        with open("new2.tsv", "w", encoding="utf-8") as wf:
+    with open("raw.tsv", encoding="utf-8") as f:
+        with open("test.tsv", "w", encoding="utf-8") as wf:
             lines=f.readlines()
             for line in lines:
                 seg = line.split("\t")
-                wf.write(f"{line}{data[seg[2]] if len(seg[2])>0 else ''}\n")
+                line = '\t'.join(seg[:-1]).replace('\n','')
+                wf.write(f"{line}{TAB}{strproc(data[seg[2]]) if len(seg[2])>0 else ''}\n")
